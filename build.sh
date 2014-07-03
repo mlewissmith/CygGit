57938fcf74a785be4b5c48198c07ede425101359
carrot <mlewissmith@users.noreply.github.com>
2014-07-04 00:14:35 +0100

    wip build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+set -e -x
 
 VERSION=1.7.9
+BUILDDIR=$(dirname $0)
+BUILDROOT=${BUILDDIR}/BUILDROOT
 
 #prep
+cd ${BUILDDIR}/git
 #git submodule update
 
 #build
-cd git
+cd ${BUILDDIR}/git
 make configure
 ./configure --prefix=/opt/git-${VERSION}
 make all
 
 #install
+cd ${BUILDDIR}/git
+make DESTDIR=${BUILDROOT}
+
+#package
+cd ${BUILDROOT}
+
