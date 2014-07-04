6d206b63624914b01272ca6c7181ac37fed85cf5
mlewissmith <mlewissmith@users.noreply.github.com>
2014-07-04 13:08:16 +0100

    Buildscript (build.sh) looks a bit like a specfile

--- build.sh
+++ build.sh
@@ -2,27 +2,52 @@
 set -e -x
 umask 0022
 
-NAME=git
-VERSION=1.9.0
-BUILDDIR=$(dirname $(readlink -e $0))
-BUILDROOT=${BUILDDIR}/BUILDROOT
+name=git
+version=1.9.0
+
+_sourcedir=$(dirname $(readlink -e $0))
+_builddir=$(dirname $(readlink -e $0))
+buildroot=${_builddir}/BUILDROOT
+
+_setup_n=${name}-${version}
 
 #prep
-rm -fr ${BUILDROOT}
-cd ${BUILDDIR}/git
-git clean -xdf
+{
+    _setup_n=git
+    rm -rf ${buildroot}
+    cd ${_builddir}
+    cd ${_setup_n}
+    git clean -xdf
+}
 
 
 #build
-cd ${BUILDDIR}/git
+{
+    cd ${_builddir}
+    cd ${_setup_n}
+}
 make configure
-./configure --prefix=/opt/git-${VERSION}
-make all
+./configure --prefix=/opt/${name}-${version}
+make -j$(nproc) all
 
 #install
-cd ${BUILDDIR}/git
-make DESTDIR=${BUILDROOT} install
+{
+    cd ${_builddir}
+    cd ${_setup_n}
+}
+make DESTDIR=${buildroot} install
+
+mkdir -p ${buildroot}/etc/profile.d
+cat <<EOF>${buildroot}/etc/profile.d/${name}-${version}.sh
+export PATH=/opt/${name}-${version}/bin:$PATH
+EOF
 
 #package
-cd ${BUILDROOT}
-tar -jcvf ${BUILDDIR}/${NAME}-${VERSION}.tar.bz2 *
+{
+    cd ${buildroot}
+}
+tar -cf ${_builddir}/${name}-${version}.${MACHTYPE}.tar *
+mkdir -p ${buildroot}/etc/setup
+tar -tf ${_builddir}/${name}-${version}.${MACHTYPE}.tar > ${buildroot}/etc/setup/${name}-${version}.lst
+gzip ${buildroot}/etc/setup/${name}-${version}.lst
+tar --append -f ${_builddir}/${name}-${version}.${MACHTYPE}.tar ./etc/setup --transform "s:^\./::"
