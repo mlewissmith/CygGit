63f3427f3c57dbdcc41d87a23edbe14ef289fdf1
mlewissmith <mlewissmith@users.noreply.github.com>
2014-07-07 13:32:29 +0100

    wip

--- _installer.sh
+++ _installer.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
+umask 0022
 
 MANIFEST=$1
 
 if [[ "${OSTYPE}" != "cygwin" ]] ; then
     echo "$OSTYPE: not cygwin"
-    exit 1
+    false
 fi
 
 ## assume /etc/setup/*.lst.gz
 zcat /etc/setup/*.lst.gz | sort -u > _installed.lst
 
-for file in `<${MANIFEST}` ; do
-    if egrep "^${file}$" _installed.lst ; then
+for file in $(<${MANIFEST}) ; do
+    if egrep -q "^${file}$" _installed.lst ; then
         if [ -d ${file} ] ; then
             # it's a directory.  Proceed
             true
@@ -25,3 +26,11 @@ done
 
 ## Verification OK.  Install...
 echo "Installing..."
+for file in $(<${MANIFEST}) ; do
+    if [[ -d $file && ! -e /${file} ]] ;
+        mkdir -v /${file}
+    else
+        cp -v ${file} /${file}
+    fi
+done
+gzip -c ${MANIFEST} > /etc/setup/${MANIFEST}.gz
